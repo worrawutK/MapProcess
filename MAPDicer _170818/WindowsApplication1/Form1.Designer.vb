31e1b15c9920edfa6ba20c2b1f604019e5f3f515
tor <sathit.chainat@gmail.com>
2019-11-25 15:20:27 +0700

    -Clear Button test

--- MAPDicer _170818/WindowsApplication1/Form1.Designer.vb
+++ MAPDicer _170818/WindowsApplication1/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,7 +20,7 @@ Partial Class Form1
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
@@ -163,7 +163,6 @@ Partial Class Form1
         Me.TableAdapterManager = New MAP_Dicer.DBxDataSetTableAdapters.TableAdapterManager()
         Me.TransactionDataTableAdapter1 = New MAP_Dicer.DBxDataSetTableAdapters.TransactionDataTableAdapter()
         Me.MAPPKGDCDataTableAdapter = New MAP_Dicer.DBxDataSetTableAdapters.MAPPKGDCDataTableAdapter()
-        Me.Button1 = New System.Windows.Forms.Button()
         Me.ContextMenuStrip1.SuspendLayout()
         CType(Me.MAPPKGDCDataBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.DBxDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
@@ -1501,15 +1500,6 @@ Partial Class Form1
         '
         Me.MAPPKGDCDataTableAdapter.ClearBeforeFill = True
         '
-        'Button1
-        '
-        Me.Button1.Location = New System.Drawing.Point(181, 617)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
-        Me.Button1.TabIndex = 257
-        Me.Button1.Text = "Button1"
-        Me.Button1.UseVisualStyleBackColor = True
-        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
@@ -1517,7 +1507,6 @@ Partial Class Form1
         Me.AutoScroll = True
         Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
         Me.ClientSize = New System.Drawing.Size(1024, 768)
-        Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.MenuStrip1)
         Me.Controls.Add(Me.tbICOff)
         Me.Controls.Add(Me.tbMakerQty)
@@ -1732,5 +1721,4 @@ Partial Class Form1
     Friend WithEvents AuthenticationUserToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents ONToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents OFFToolStripMenuItem As ToolStripMenuItem
-    Friend WithEvents Button1 As Button
 End Class
--- MAPDicer _170818/WindowsApplication1/Form1.vb
+++ MAPDicer _170818/WindowsApplication1/Form1.vb
@@ -2152,7 +2152,5 @@ inputQr:
         file_Log.Close()
     End Sub
 
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        FrmInputCarrierTranfer.ShowDialog()
-    End Sub
+
 End Class
