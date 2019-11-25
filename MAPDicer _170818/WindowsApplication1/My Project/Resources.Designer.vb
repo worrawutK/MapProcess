f6f595df223d8ed362651cf1ec432b079e7f18f1
tor <sathit.chainat@gmail.com>
2019-11-25 13:53:24 +0700

    Add FrmInputCarrierTranfer

--- MAPDicer _170818/WindowsApplication1/My Project/Resources.Designer.vb
+++ MAPDicer _170818/WindowsApplication1/My Project/Resources.Designer.vb
@@ -99,5 +99,15 @@ Namespace My.Resources
                 Return CType(obj,System.Drawing.Bitmap)
             End Get
         End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
+        Friend ReadOnly Property TranGIF() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("TranGIF", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
     End Module
 End Namespace
